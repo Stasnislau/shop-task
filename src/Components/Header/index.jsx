572df818e047b0d32927f60414574ad6cbaff14e
Stanislau Ryzhkou <stanislaurv@gmail.com>
2022-12-03 00:31:10 +0100

    small bugfixes

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -9,34 +9,100 @@ class HeaderElement extends React.Component {
     super(props);
     this.state = {
       name: this.props.name,
-      chosen: false,
+      chosen: this.props.chosen,
+      changeChosen: this.props.changeChosen,
     };
   }
   render() {
     return (
-        <button className="navigation-element-button">{this.state.name}</button>
+      <button
+        className={
+          this.state.chosen
+            ? "navigation-element-button chosen-navigation-button"
+            : "navigation-element-button"
+        }
+        onClick={() => {
+          this.state.changeChosen(this.state.name);
+        }}
+      >
+        {this.state.name}
+      </button>
     );
   }
 }
-const names = ["Women", "Men", "Kids"];
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elements: [
+        {
+          name: "Women",
+          chosen: false,
+        },
+        {
+          name: "Men",
+          chosen: false,
+        },
+        {
+          name: "Kids",
+          chosen: false,
+        },
+      ],
+    };
+  }
+  changeChosen = (name) => {
+    const newElements = [];
+    this.state.elements.map((element, index) => {
+      if (element.name === name) {
+        if (element.chosen) {
+          newElements.push({
+            name: element.name,
+            chosen: false,
+          });
+        } else {
+          newElements.push({
+            name: element.name,
+            chosen: true,
+          });
+        }
+      } else {
+        newElements.push({
+          name: element.name,
+          chosen: false,
+        });
+      }
+    });
+    this.setState({ elements: newElements });
+    console.log(this.state.elements);
+  };
   render() {
     return (
       <div className="header">
         <div className="header-navigation">
-          {names.map((name, index) => {
-            return <HeaderElement name={name} />;
+          {this.state.elements.map((name, index) => {
+            return (
+              <HeaderElement
+                name={this.state.elements[index].name}
+                chosen={this.state.elements[index].chosen}
+                changeChosen={this.changeChosen}
+              />
+            );
           })}
-          
         </div>
 
         <div className="brand-icon">
           <img src={Logo} alt="Brand Icon" />
         </div>
         <div className="header-action-bar">
-            <button className="currency-button" > $      <img src={Arrow}/> </button>
-            <button className="cart-button"> <img src={Cart}/></button>
-          </div>
+          <button className="currency-button">
+            {" "}
+            $ <img src={Arrow} />{" "}
+          </button>
+          <button className="cart-button">
+            {" "}
+            <img src={Cart} />
+          </button>
+        </div>
       </div>
     );
   }
--- src/Components/ItemCard/index.jsx
+++ src/Components/ItemCard/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import CircleIcon from "./../Logo/CircleIcon.png";
 class ItemCard extends React.Component {
   constructor(props) {
     super(props);
@@ -8,17 +9,55 @@ class ItemCard extends React.Component {
       price: this.props.price,
       image: this.props.image,
       currency: this.props.currency,
+      outOfStock: this.props.outOfStock,
       chosen: false,
     };
   }
   render() {
     return (
-      <button className="item-card">
+      <button
+        className={
+          this.state.outOfStock
+            ? "out-of-stock item-card boundless-button"
+            : "item-card boundless-button"
+        }
+        onMouseEnter={() => {
+          this.setState({ chosen: true });
+        }}
+        onMouseLeave={() => {
+          this.setState({ chosen: false });
+        }}
+        onClick={() => {
+          this.setState({ outOfStock: !this.state.outOfStock }); // This is just for the demo
+          return null; // TODO: Add functionality
+        }}
+      >
         <div className="item-card-image">
-          <img className="item-card-image" src={this.state.image} alt="Item Image" />
+          <img
+            className="item-card-image"
+            src={this.state.image}
+            alt="Item Image"
+          />
+          {this.state.outOfStock ? (
+            <span className="out-of-stock-text">Out of stock</span>
+          ) : null}
+          {this.state.chosen && !this.state.outOfStock ? (
+            <button className="boundless-button">
+              <img
+                className="item-card-icon"
+                src={CircleIcon}
+                alt="Circle Icon"
+              />
+            </button>
+          ) : null}
         </div>
         <div className="item-card-name">{this.state.name}</div>
-        <div className="item-card-price"><b>{this.state.currency}{this.state.price}</b></div>
+        <div className="item-card-price">
+          <b>
+            {this.state.currency}
+            {this.state.price}
+          </b>
+        </div>
       </button>
     );
   }
